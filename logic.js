f5e13767181ae28406d622faeed7a7035fba0506
aemassa <annaemassa@gmail.com>
2015-07-08 22:52:56 -0400

    Adds initial JS logic to create an empty board and determine a winner.

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
 /* Game Logic
 - To start, divs will not contain any values until a user assigns a value (X or O).
 
- a | b | c
- d | e | f
- g | h | i
+ 0 | 1 | 2
+ 3 | 4 | 5
+ 6 | 7 | 8
 
  '' | '' | ''
  '' | '' | ''
@@ -11,7 +11,7 @@
 
 - Optional: A player can click on "Let's play Tic-Tac-Toe", which will prompt the following question in the div below "Let's play Tic-Tac-Toe" and above the game board:
 
-- In the div just above the game board, a question and two buttons will appear: The question will be, "Would you like to be X or O?"  The buttons will say either "X" or "O". If the player clicks the "X" button, the buttons will diseapper and the question will change to the message "It's your turn, X!" Player 2 is automatically assigned the other value (in this example, O).
+- In the div just above the game board, a question and two buttons will appear: The question will be, "Would you like to be X or O?"  The buttons will say either "X" or "O". If the player clicks the "X" button, the buttons will disappear and the question will change to the message "It's your turn, X!" Player 2 is automatically assigned the other value (in this example, O).
 
 - First, Player 1 (X) clicks one of the empty game board squares, that square will be set to 'X'.  Example:
 
@@ -35,40 +35,109 @@
 
 - The count of O's on the board will increase by 1.
 
+- To determine whose turn it is:
+  Option 1:
+    - Create a "counter" variable and set it to 0. Create a function to increase "counter" by 1 player move, increase "counter" by 1.  If the value of "counter" is odd, it's player 1's turn. If even, it's player 2's turn.
+
 - Then it's Player 1 (X)'s turn again.  Repeat turns until there is a winner
 
 - To determine winner:
 
 either X or O:
-  - wins horizontally: [a, b, c] or [d, e, f] or [g, h, i] will be either all X or all O.
-  - wins vertically: [a, d, g] or [b, e, h] or [c, f, i] will either by all X or all O.
-  - wins diagonally: [a, e, i] or [c, e, g] will be either all X or all O.
+  - wins horizontally: [0, 1, 2] or [3, 4, 5] or [6, 7, 8] will be either all X or all O.
+  - wins vertically: [0, 3, 6] or [1, 4, 7] or [2, 5, 8] will either by all X or all O.
+  - wins diagonally: [0, 4, 8] or [2, 4, 6] will be either all X or all O.
 
 - If any of the above combinations include all X's or all O's, return winner and stop game.  If X wins, the text at top changes to "Game over!  X has won."
 
 - Optional: After winner is determined, add a point to the score section for that player.
 
 Other:
-- new game button clears board, sets all squares back to null. */
+- new game button clears board, sets all squares back to null.
+
+Below: Console Game Logic */
 
 'use strict'
 
-var board = [['', '', ''], ['', '', ''], ['', '', '']];
+// //The first section of this file defines our variables.
 
-//board;
+// //Creates empty board and stores it in a variable called "board":
 
-var playerMove = function (player, row, col) {
-    board[row][col] = player;
-    return board;
-}
+// var board = [['', '', ''],
+//             ['', '', ''],
+//             ['', '', '']];
 
-playerMove ('X', 1, 1);
+// //Sets the variable "winner":
+// var winner;
 
-var getPieceAt = function (row, col) {
-    if (board[row][col] !== null) {
-        return board[row][col];
+
+
+// var playerMove = function (player, row, col) {
+//     board[row][col] = player;
+//     return board;
+// }
+
+// playerMove ('X', 1, 1);
+
+// var getPieceAt = function (row, col) {
+//     if (board[row][col] !== null) {
+//         return board[row][col];
+//     }
+//     else return null;
+// }
+
+// getPieceAt(1, 2);
+
+var board = [['a1', 'b1', 'c1'],
+            ['a2', 'b2', 'c2'],
+            ['a3', 'b3', 'c3']];
+
+var currentTurn = "X";
+
+var changeTurn = function(piece) {
+    if (currentTurn == "X") {
+      //$("#" + piece).html("X");
+            this.checkWin();
+      currentTurn = "O";
+    } else {
+      //$("#" + piece).html("O");
+      this.checkWin();
+      currentTurn = "X";
     }
-    else return null;
-}
+  };
 
-getPieceAt(1, 2);
+var checkRows = function() {
+      //checks for horizontal wins first
+      if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+      else if (board[1][0] == player && board[1][1] == player && board[1][2] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+      else if (board[2][0] == player && board[2][1] == player && board[2][2] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+};
+
+var checkCols = function() {
+      //checks for vertical wins
+       if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+      else if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+      else if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+};
+
+var checkDiag = function() {
+      //checks for diagonal wins
+       if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+      else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+          console.log(currentTurn + 'wins!');
+      }
+};
