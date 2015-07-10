f931760fd174c3358affb96e2e0f8ccefef9f9ce
aemassa <annaemassa@gmail.com>
2015-07-09 20:07:33 -0400

    Modified the resetBoard function so that it resets the counter to 0.

--- logic.js
+++ logic.js
@@ -1,57 +1,37 @@
 /* Game Logic
 - To start, divs will not contain any values until a user assigns a value (X or O).
-
  0 | 1 | 2
  3 | 4 | 5
  6 | 7 | 8
-
  '' | '' | ''
  '' | '' | ''
  '' | '' | ''
-
 - Optional: A player can click on "Let's play Tic-Tac-Toe", which will prompt the following question in the div below "Let's play Tic-Tac-Toe" and above the game board:
-
 - In the div just above the game board, a question and two buttons will appear: The question will be, "Would you like to be X or O?"  The buttons will say either "X" or "O". If the player clicks the "X" button, the buttons will disappear and the question will change to the message "It's your turn, X!" Player 2 is automatically assigned the other value (in this example, O).
-
 - First, Player 1 (X) clicks one of the empty game board squares, that square will be set to 'X'.  Example:
-
 '' | '' | ''
 '' | 'x' | ''
 '' | '' | ''
-
 - The location of the new X will be stored (on the board? in a new array or object representing the board?).
-
 - The count of X's on the board will increase by 1.
-
 - Optional: The "It's your turn, X!" message will change to "It's your turn, O!" once player X has completed their move (and the count of X's on the board increases by 1).
-
 - Player 2 (O), will click on a square on the board (where there is NOT already an X or an O) set another square to 'O'. Example:
-
 '' | '' | 'o'
 '' | 'x' | ''
 '' | '' | ''
-
 - The location of this new O will be stored (in the same board/object/whatever as the X above).
-
 - The count of O's on the board will increase by 1.
-
 - To determine whose turn it is:
   Option 1:
     - Create a "counter" variable and set it to 0. Create a function to increase "counter" by 1 player move, increase "counter" by 1.  If the value of "counter" is odd, it's player 1's turn. If even, it's player 2's turn.
-
 - Then it's Player 1 (X)'s turn again.  Repeat turns until there is a winner
-
 - To determine winner:
-
 either X or O:
   - wins horizontally: [0, 1, 2] or [3, 4, 5] or [6, 7, 8] will be either all X or all O.
   - wins vertically: [0, 3, 6] or [1, 4, 7] or [2, 5, 8] will either by all X or all O.
   - wins diagonally: [0, 4, 8] or [2, 4, 6] will be either all X or all O.
-
 - If any of the above combinations include all X's or all O's, return winner and stop game.  If X wins, the text at top changes to "Game over!  X has won."
-
 - Optional: After winner is determined, add a point to the score section for that player.
-
 Other:
 - new game button clears board, sets all squares back to null.
  */
@@ -169,6 +149,7 @@ var resetBoard = function(){
           ['', '', ''],
           ['', '', '']];
   $('#board > div').html('');
+  counter = 0;
 }
 
 //next steps:
@@ -205,3 +186,9 @@ $(document).ready(function(){
 
 //row-0 col-0
 });
+
+//x win - complete
+//o win - complete
+//column win - complete
+//row win - complete
+//diag win - complete
